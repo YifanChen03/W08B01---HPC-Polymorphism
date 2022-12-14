f7540e74db36c6fb01bc55234a1732fad27fd238
cyanfhine <yifan03.chen@tum.de>
2022-12-14 16:32:28 +0100

    supermuc testimplementation

--- src/pgdp/poly/HPCPoly.java
+++ src/pgdp/poly/HPCPoly.java
@@ -175,7 +175,7 @@ public class HPCPoly {
 		// TASK 2 (start)
 
 		// TODO Write the required code between these tags. Do not remove the tags!
-		return null;
+		return new SuperMUC();
 
 		// TASK 2 (end)
 	}
